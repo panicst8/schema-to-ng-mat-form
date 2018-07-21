b4af9ef1f442f7a0dbc21e6b8294640d88d66356
Chris Milford <panicst8@yahoo.com>
2018-07-21 02:14:38 -0500

    close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  jsonFormSchema = 'empty';
+  yourJsonSchema = 'empty';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    console.log('init');
+    const exampleURL = `assets/kitchen.json`;
+    this.http.get(exampleURL).subscribe((schema) => {
+      console.log(schema);
+      // this.jsonFormSchema = schema;
+      // this.generateForm(this.jsonFormSchema);
+    });
+  }
+  generateForm(schema: any): any {
+    console.log(schema);
+  }
+
+  yourOnSubmitFn(e) {
+    console.log(e);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  JsonSchemaFormModule,
+  MaterialDesignFrameworkModule,
+} from 'projects/json-schema-form/src/public_api';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // JsonSchemaFormComponent
   ],
   imports: [
-    BrowserModule
+    FlexLayoutModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MaterialModule,
+    MaterialDesignFrameworkModule,
+    JsonSchemaFormModule.forRoot(MaterialDesignFrameworkModule),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
